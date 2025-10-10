6cc2aa7f09899f710a92db5ed980372849558ae7
Eckhart Spalding <eckhartspalding@staff-net-h3-1-a-1731.intern.ethz.ch>
2025-10-10 14:44:02 +0200

    fixed display bug that made it seem there were no psfs

--- playing_with_scopesim/eckhart_IMG_OPT_04_psf_image_quality_sim.py
+++ playing_with_scopesim/eckhart_IMG_OPT_04_psf_image_quality_sim.py
@@ -21,7 +21,7 @@
 # need to achieve a significantly higher SNR for accurate measurement of the FWHM, and better control of
 # calibration and flat fielding errors for measurement of the Encircled Energy. The flat field will be derived
 # from IMG-RAD-04.
- 
+
 import numpy as np
 from astropy.io import fits
 from astropy import units as u
@@ -45,97 +45,86 @@ sim.link_irdb("../../../")
 # simulate observations with METIS (comment this out if packages already exist)
 #sim.download_packages(["METIS", "ELT", "Armazones"])
 
-
-
-# see all filters
-#metis["filter_wheel"].filters.keys()
-
 def generate_psf_image_quality_data(fp_mask, obs_filter, dither_position_array, obs_mode):
 
     # set up instrument
     cmd = sim.UserCommands(use_instrument='METIS', set_modes=[obs_mode])
     metis = sim.OpticalTrain(cmd)
 
-    #metis["chop_nod"].include = True # allow chopping
     metis.effects.pprint_all()
     wcu = metis['wcu_source']
-    #cfo = metis['cfo_source']
+
     bb_temp = 1000 * u.K
     NDIT, EXPTIME = 1, 0.2
 
-    # DO NOT REMOVE THIS 'for'! this option has to be present to avoid a bug that gets triggered in the 'else' case; don't know why
-    for mode in ["close BB aperture first for background", "don't close BB aperture first for background"]:
+    print('Generating ' + str(fp_mask)) 
+    wcu.set_fpmask(fp_mask)
 
-        print('Generating ' + str(fp_mask)) 
-        wcu.set_fpmask(fp_mask)
+    print('Closing WCU BB aperture first for background ...')
+    # background
+    wcu.set_bb_aperture(value = 0.0)
+    metis.observe()
+    outhdul_off = metis.readout(ndit = NDIT, exptime = EXPTIME)[0]
+    background = outhdul_off[1].data
 
-        if mode == "close BB aperture first for background":
-            # DO NOT REMOVE THIS! this option has to be present to avoid a bug that gets triggered in the 'else' case; don't know why
-            print('Closing WCU BB aperture first...')
-            # background
-            wcu.set_bb_aperture(value = 0.0)
-            metis.observe()
-            outhdul_off = metis.readout(ndit = NDIT, exptime = EXPTIME)[0]
-            background = outhdul_off[1].data
-        else:
+    dither_num_array = [0, 1] # 0: no dither, 1: dither
+    wcu.set_bb_aperture(value = 1.0) # open BB source
 
-            dither_num_array = [0, 1] # 0: no dither, 1: dither
-            wcu.set_bb_aperture(value = 1.0) # open BB source
+    metis["filter_wheel"].change_filter(obs_filter)
 
-            metis["filter_wheel"].change_filter(obs_filter)
+    for dither_pos in dither_position_array:
 
-            for dither_pos in dither_position_array:
+        print('--------------------------------')
+        print('Current WCU FP mask:', wcu.fpmask)
+        print('Current Observing filter:', metis["filter_wheel"].current_filter)
+        print('Current WCU PP mask:', metis['pupil_masks'].current_mask)
 
-                # just make background a bunch of zeros for now to get around aforementioned bug
-                outhdul_off = metis.readout(ndit = NDIT, exptime = EXPTIME)[0]
-                background = np.zeros_like(outhdul_off[1].data)
+        # dither by shifting the FP mask
+        # (note these shifts are absolute, not relative)
+        wcu.set_fpmask(fp_mask, angle=0, shift=dither_pos)
 
-                print('--------------------------------')
-                print('Current WCU FP mask:', wcu.fpmask)
-                print('Current Observing filter:', metis["filter_wheel"].current_filter)
-                print('Current WCU PP mask:', metis['pupil_masks'].current_mask)
+        print('Opening WCU BB aperture...')
 
-                # dither by shifting the FP mask
-                # (note these shifts are absolute, not relative)
-                wcu.set_fpmask(fp_mask, angle=0, shift=dither_pos)
+        metis.observe()
+        outhdul = metis.readout(ndit = NDIT, exptime = EXPTIME)[0]
+        #outhdul[1].data
+        #outhdul.writeto(f"IMG_OPT_02_wcu_focal_plane_{mask}.fits", overwrite=True)
 
-                print('Opening WCU BB aperture...')
+        ipdb.set_trace()
 
-                metis.observe()
-                outhdul = metis.readout(ndit = 1, exptime = 0.2)[0]
-                #outhdul[1].data
-                #outhdul.writeto(f"IMG_OPT_02_wcu_focal_plane_{mask}.fits", overwrite=True)
+        # background-subtract
+        bckgd_subted = outhdul[1].data - background
 
-                ipdb.set_trace()
+        ipdb.set_trace()
 
-                # detector
-                plt.clf()
-                zscale = ZScaleInterval()
-                vmin, vmax = zscale.get_limits(outhdul[1].data)
-                plt.imshow(outhdul[1].data - background, origin='lower', vmin=vmin, vmax=vmax)
-                plt.title(f'Readout\nWCU FP mask: ' + str(fp_mask) + '\n' + 'Observing filter: ' + str(obs_filter) + '\n' + 'BB temp: ' + str(bb_temp))
-                plt.tight_layout()
-                plt.show()
-                plt.close()
+        # detector
+        plt.clf()
+        zscale = ZScaleInterval()
+        vmin, vmax = zscale.get_limits(bckgd_subted)
+        plt.imshow(bckgd_subted, origin='lower', vmin=vmin, vmax=vmax)
+        plt.title(f'Readout\nWCU FP mask: ' + str(fp_mask) + '\n' + 'Observing filter: ' + str(obs_filter) + '\n' + 'BB temp: ' + str(bb_temp))
+        plt.tight_layout()
+        plt.show()
+        plt.close()
 
-                # histogram
-                plt.clf()
-                plt.hist(outhdul[1].data.ravel(), bins=200)
-                plt.title('Bckgd-subtracted histogram; WCU FP mask: ' + str(fp_mask))
-                plt.tight_layout()
-                plt.show()
-                plt.close()
+        # histogram
+        plt.clf()
+        plt.hist(bckgd_subted.ravel(), bins=200)
+        plt.title('Bckgd-subtracted histogram; WCU FP mask: ' + str(fp_mask))
+        plt.tight_layout()
+        plt.show()
+        plt.close()
 
-                # save to FITS file
-                file_name = 'IMG_OPT_04_wcu_focal_plane_' + str(fp_mask) + '.fits'
-                outhdul.writeto(file_name, overwrite=True)
-                print('Saved readout without aberrations to ' + file_name)
+        # save to FITS file
+        file_name = 'IMG_OPT_04_wcu_focal_plane_' + str(fp_mask) + '.fits'
+        outhdul.writeto(file_name, overwrite=True)
+        print('Saved readout without aberrations to ' + file_name)
 
-                # do a hackneyed aberration: blurring made to look like defocus 
-                file_name = 'IMG_OPT_04_wcu_focal_plane_' + str(fp_mask) + '_blur.fits'
-                outhdul[1].data = scipy.ndimage.gaussian_filter(outhdul[1].data, sigma=3)
-                outhdul.writeto(file_name, overwrite=True)
-                print('Saved readout with aberrations to ' + file_name)
+        # do a hackneyed aberration: blurring made to look like defocus 
+        file_name = 'IMG_OPT_04_wcu_focal_plane_' + str(fp_mask) + '_blur.fits'
+        outhdul[1].data = scipy.ndimage.gaussian_filter(outhdul[1].data, sigma=3)
+        outhdul.writeto(file_name, overwrite=True)
+        print('Saved readout with aberrations to ' + file_name)
 
 
 def main():
@@ -155,18 +144,16 @@ def main():
     # make as many dither positions as desired
     dither_position_array = [(0, 0), (1, 0), (0, 1), (1, 1)]
 
+    # LM band
     for fp_mask in lm_fpmasks_list:
         for obs_filter in lm_filters_list:
             generate_psf_image_quality_data(fp_mask, obs_filter, dither_position_array=dither_position_array, obs_mode='wcu_img_lm')
 
+    # N band
+    for fp_mask in n_fpmasks_list:
+        for obs_filter in n_filters_list:
+            generate_psf_image_quality_data(fp_mask, obs_filter, dither_position_array=dither_position_array, obs_mode='wcu_img_n')
+
 
 if __name__ == "__main__":
     main()
-
-
-'''
-CONTINUE HERE: 
-- how to treat N-band correctly, too? (note this will require chopping; see Overleaf)
-- x how to dither?
-- label all plots and write them out when they are seen to be well-behaved
-'''
