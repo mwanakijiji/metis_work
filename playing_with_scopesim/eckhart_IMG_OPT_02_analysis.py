53214a8d206b9dd794faab686c2573b72eed8104
Eckhart Spalding <eckhartspalding@staff-net-vpn-dhcp-2139.intern.ethz.ch>
2025-11-08 13:36:00 +0100

    distances lookup table offers arcsec

--- playing_with_scopesim/eckhart_IMG_OPT_02_analysis.py
+++ playing_with_scopesim/eckhart_IMG_OPT_02_analysis.py
@@ -19,7 +19,7 @@ from astropy.visualization import ZScaleInterval
 import ipdb
 
 
-def fov(file_names_fov_tl, file_names_fov_tr, file_names_fov_bl, file_names_fov_br):
+def fov(file_names_fov_tl, file_names_fov_tr, file_names_fov_bl, file_names_fov_br, scaling_reln):
     ##################################################################
     ## TEST 1: FOV
     ## read in frames with PSF at each corner and see how far apart they are in pixel space
@@ -67,14 +67,11 @@ def fov(file_names_fov_tl, file_names_fov_tr, file_names_fov_bl, file_names_fov_
     del_y_left = np.abs(y_tl-y_bl)
     del_y_right = np.abs(y_tr-y_br)
 
-    print('! ---------- USING A PLACEHOLDER SCALING RELN ---------- !')
-    scaling_reln = 0.01046 # [arcsec/pix]
-
     fov_calc = scaling_reln * np.mean(np.array(del_x_top,del_x_bottom,del_y_left,del_y_right))
     print('FOV:', fov_calc, '[arcsec]')
 
 
-def plate_scale(file_name_grid):
+def plate_scale(file_name_grid, scaling_reln):
 
     grid_frame = fits.open(file_name_grid)
     grid_data = grid_frame[1].data
@@ -103,6 +100,11 @@ def plate_scale(file_name_grid):
     ipdb.set_trace()
     #guesses_grid = np.array(())
 
+    # for debugging
+    print('! ---------- debugging --------- !')
+    x_grid = np.array([3.0, 3.0, 3.1, 3.3])
+    y_grid = np.array([13.0, 13.0, 13.1, 13.3])
+
     # find the grid centroids
     x_grid, y_grid = centroid_sources(grid_data, xpos=coords_guesses_x_all, ypos=coords_guesses_y_all)
 
@@ -113,7 +115,13 @@ def plate_scale(file_name_grid):
 
     permutation_distance_lookup = {}
     for (idx1, coord1), (idx2, coord2) in permutations(enumerate(coords_centroided_all), 2):
-        permutation_distance_lookup[(tuple(coord1), tuple(coord2))] = pairwise_distance_matrix[idx1, idx2]
+        distance_pix = pairwise_distance_matrix[idx1, idx2]
+        permutation_distance_lookup[(tuple(coord1), tuple(coord2))] = {
+            "distance_pix": distance_pix,
+            "distance_arcsec": distance_pix * scaling_reln,
+        }
+
+    
 
     ipdb.set_trace()
 
@@ -135,11 +143,16 @@ def main():
     # the files for constraining stray light (pinhole)
     file_name_pinhole = stem + 'stray_light/IMG_OPT_04_plate_scale_grid_image_pinhole_lm_Lp.fits'
 
+    # true scaling relation between arcseconds and pixels (measured, not assumed)
+    # value and units may change depending on experimental setup
+    print('! ---------- USING A PLACEHOLDER SCALING RELN ---------- !')
+    scaling_reln = 0.01046 # [arcsec/pix]
+
     # check FOV
-    #fov(file_names_fov_tl, file_names_fov_tr, file_names_fov_bl, file_names_fov_br)
+    #fov(file_names_fov_tl, file_names_fov_tr, file_names_fov_bl, file_names_fov_br, scaling_reln)
 
     # check plate scale 
-    plate_scale(file_name_grid)
+    plate_scale(file_name_grid, scaling_reln)
 
 
 if __name__ == "__main__":
